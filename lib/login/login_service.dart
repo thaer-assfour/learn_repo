797c812c96f69108f37fe7d66f36a4101cd60204
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2020-10-30 09:40:37 +0200

    added task 01 skeleton

---
+++ lib/login/login_service.dart
@@ -0,0 +1,6 @@
+// Requests and API Logic Happens Here.
+
+class LoginService {
+  // Post Login Details Here, and Publish Results to LoginUIScreen
+  // NOTE: HTTP_CLIENT ONLY HERE
+}
---
+++ lib/login/login_ui.dart
@@ -0,0 +1,14 @@
+// Design Goes Here
+import 'package:flutter/material.dart';
+
+
+class LoginUIScreen extends StatefullWidget {
+
+}
+
+class _LoginUIScreenState extends State<LoginUIScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
